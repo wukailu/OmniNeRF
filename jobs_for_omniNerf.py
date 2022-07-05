7fadc728a9590a61cc60a22ec92ca94977253a45
wuvin <wuvin@outlook.com>
2022-07-06 01:47:54 +0800

    Update jobs_for_omniNerf.py

--- jobs_for_omniNerf.py
+++ jobs_for_omniNerf.py
@@ -11,27 +11,28 @@ templates = {
 }
 
 
-def StandFordArea3():
-    files = os.listdir('/data/Standford/area_3/pano/rgb/')
+def StandFordArea():
+    area_id = '1'
+    files = os.listdir(f'/data/Standford/area_{area_id}/pano/rgb/')
     file = random_params(files)
     params = templates['standford']
-    iters = 30000
+    iters = 50000
     params.update({
-        'project_name': "Area3",
+        'project_name': f"Area{area_id}",
         'iters': iters,
         'i_weights': iters,
         'i_testset': iters,
         'i_video': iters,
-        "rgb_path": '/data/Standford/area_3/pano/rgb/' + file,
-        "depth_path": '/data/Standford/area_3/pano/depth/' + file.replace("_rgb", "_depth"),
+        "rgb_path": f'/data/Standford/area_{area_id}/pano/rgb/' + file,
+        "depth_path": f'/data/Standford/area_{area_id}/pano/depth/' + file.replace("_rgb", "_depth"),
     })
     return random_params(params)
 
 
 def params_for_Nerf():
-    params = StandFordArea3()
+    params = StandFordArea()
     return params
 
 
 if __name__ == "__main__":
-    submit_jobs(params_for_Nerf, 'run_nerf.py', number_jobs=88, job_directory='.')
+    submit_jobs(params_for_Nerf, 'run_nerf.py', number_jobs=300, job_directory='.')
